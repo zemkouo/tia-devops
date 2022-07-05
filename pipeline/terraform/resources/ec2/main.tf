0a793a2147218baee30f595041113373f5a5469d
Tia Leonard <leonardtia57@yhoo.fr>
2022-07-05 10:30:37 -0500

    docker

--- pipeline/terraform/resources/ec2/main.tf
+++ pipeline/terraform/resources/ec2/main.tf
@@ -1,9 +1,31 @@
-provider "aws" {
-  region = "us-east-1"
+# Terraform Block
+terraform {
+  required_version = "> 0.14"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+  # Adding Backend as S3 for Remote State Storage
+  backend "s3" {
+    bucket = "tia-terraform-state-backend/vpc"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    # dynamodb_table = "terraform_state"
+  }
 }
 
-resource "aws_instance" "instance-1" {
-  ami           = "ami-052efd3df9dad4825"
-  instance_type = "t2.micro"
-  count         = 1
+# Provider Block
+provider "aws" {
+  region  = "us-east-1"
+  profile = "default"
+}
+
+resource "aws_vpc" "vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+  tags = {
+    Name = "vpc-test"
+  }
 }
